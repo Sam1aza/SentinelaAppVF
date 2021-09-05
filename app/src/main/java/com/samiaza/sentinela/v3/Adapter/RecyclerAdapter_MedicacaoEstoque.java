881be63cbb2b7c6e5778f8e72b94e7ac41095a1f
Wallisson <wallisson.salatiel@gmail.comm>
2021-09-05 14:16:45 -0300

    Alteraçõs de layout

---
+++ app/src/main/java/com/samiaza/sentinela/v3/Adapter/RecyclerAdapter_MedicacaoEstoque.java
@@ -0,0 +1,4 @@
+package com.samiaza.sentinela.v3.Adapter;
+
+public class RecyclerAdapter_MedicacaoEstoque {
+}
---
+++ app/src/main/java/com/samiaza/sentinela/v3/MedicamentoEstoque.java
@@ -0,0 +1,14 @@
+package com.samiaza.sentinela.v3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class medicamentos_estoque extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicamentos_estoque);
+    }
+}
